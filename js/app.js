a14b6a88861b8afe72bb93ba3b53e931d54bed01
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-13 16:12:54 +0200

    style applied on the "done" todo item doesn't work

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
+let toDoList = [
+    {
+        text: "Clean the room",
+        done: false,
+    },
+    {
+        text: "Buy a new car",
+        done: false,
+    },
+    {
+        text: "Do homework",
+        done:false,
+    },
+    {
+        text: "Finish the exercise",
+        done: false,
+    },
+]
+
 new Vue ({
 
     el: "#app",
     data: {
-
+        toDoListItem: toDoList,
     },
-    methods: {
-        
-    }
 })
+
+/* 
+Print inside a list, a item for every todo.
+If done propriety = true -> show the text barred
+
+*/
