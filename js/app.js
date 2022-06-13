c666e74fd4ef654bd6d829cc0260fa0b74ba325c
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-13 17:44:52 +0200

    bonus section completed

--- js/app.js
+++ js/app.js
@@ -41,10 +41,3 @@ new Vue ({
     }
 })
 
-/* 
-Predisporre un campo di input 
-testuale (sempre visibile) e un pulsante 
-“aggiungi”: cliccando sul pulsante, il testo
- digitato viene letto e utilizzato per creare un nuovo todo, 
- che quindi viene aggiunto alla lista dei todo esistenti.
-*/
